c27d0505bea8a0684b4fa41387e646e3ec7e86a1
Melinda622 <melindasahouli@hotmail.com>
2021-12-12 21:55:19 +0100

    Soccer.java and SoccerTest.java created

---
+++ src/main/java/day04/Soccer.java
@@ -0,0 +1,64 @@
+package day04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Soccer {
+
+    public List<String> readFootball() {
+
+        List<String> football = new ArrayList<>();
+        Path path = Paths.get("src/main/resources/Football.txt");
+
+        try {
+            football = Files.readAllLines(path);
+            return football;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return football;
+    }
+
+
+    public List<String> transformFootball() {
+
+        List<String> formattedFootball = new ArrayList<>();
+
+        for (int i = 1; i < 18; i++) {
+            formattedFootball.add(readFootball().get(i));
+        }
+
+        for (int i = 19; i < readFootball().size(); i++) {
+            formattedFootball.add(readFootball().get(i));
+        }
+
+        return formattedFootball;
+    }
+
+    public String findSmallestDifference() {
+
+        int scoredGoals = Integer.parseInt(transformFootball().get(0).substring(42, 45).trim());
+
+        int receivedGoals = Integer.parseInt(transformFootball().get(0).substring(49, 52).trim());
+
+        String minTeam = "";
+
+        int minDifference = 1000;
+
+        for (String s : transformFootball()) {
+            String team = s.substring(6, 18).trim();
+            scoredGoals = Integer.parseInt(s.substring(42, 45).trim());
+            receivedGoals = Integer.parseInt(s.substring(49, 52).trim());
+            int difference = Math.abs(scoredGoals - receivedGoals);
+            if (difference < minDifference) {
+                minDifference = difference;
+                minTeam = team;
+            }
+        }
+        return minTeam;
+    }
+}
---
+++ src/test/java/day04/SoccerTest.java
@@ -0,0 +1,15 @@
+package day04;
+
+import day05.Soccer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoccerTest {
+
+    @Test
+    void TestSoccer(){
+        day05.Soccer soccer=new Soccer();
+        assertEquals("Aston_Villa",soccer.findSmallestDifference());
+    }
+}
